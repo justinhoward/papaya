91d03d76ff24737e8720c59f2c82a9d9b2d88a05
Justin Howard <jmhoward0@gmail.com>
2014-11-19 22:25:46 -0800

    added release build task
    - created build task that builds essential files to a release directory
    - there is a task for a zip archive and one for a tarball

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,20 @@
 'use strict';
+var pkg = require('./package');
 var gulp = require('gulp');
 var umd = require('gulp-umd');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var zip = require('gulp-zip');
+var tar = require('gulp-tar');
+var gzip = require('gulp-gzip');
 
 var paths = {
     src: './papaya.js',
-    dist: './dist'
+    dist: './dist',
+    release: './release',
+    release_files: ['./README.md', './LICENSE.txt', './papaya.js', './dist/*'],
+    release_zip: pkg.name + '-' + pkg.version + '.zip',
+    release_tar: pkg.name + '-' + pkg.version + '.tar'
 };
 
 gulp.task('default', ['build']);
@@ -18,4 +26,19 @@ gulp.task('build', function() {
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
         .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('release', ['release-tar', 'release-zip']);
+
+gulp.task('release-tar', function() {
+    return gulp.src(paths.release_files, {base: './'})
+        .pipe(tar(paths.release_tar))
+        .pipe(gzip())
+        .pipe(gulp.dest(paths.release));
+});
+
+gulp.task('release-zip', function() {
+    return gulp.src(paths.release_files, {base: './'})
+        .pipe(zip(paths.release_zip))
+        .pipe(gulp.dest(paths.release));
 });
